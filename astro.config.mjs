f19514993b145f80d29a5f8ebdc9d3afea47f0f8
Jefferson-chidiox <Jeffersonchidiox@gmail.com>
2024-06-24 08:41:14 +0100

    comleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  site: "https://malone.vercel.app",
+  integrations: [sentry(), spotlightjs(), preact()]
 });
